7426a5342106426290917b830aecd3ec2b3d0c2a
Kenichi Okiebisu <a01056715@gmail.com>
2021-02-13 22:27:25 -0800

    chore

--- packages/atoms/lib/components/button/index.tsx
+++ packages/atoms/lib/components/button/index.tsx
@@ -4,7 +4,7 @@ import "../../styles.css";
 export const Button = () => {
   return (
     <button
-      onClick={() => alert("The button is clicked")}
+      onClick={() => alert("The button is clicked yoyo")}
       className="px-5 py-3 bg-red-400 rounded-lg text-white font-bold"
     >
       Button
